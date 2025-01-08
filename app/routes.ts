34ade87582f2448dc2cd8b80dc2792b390f7e2b1
Diwash <dopkhrel111@gmail.com>
2025-01-08 14:09:31 +0545

    changing the route for company page

--- app/routes.ts
+++ app/routes.ts
@@ -9,7 +9,7 @@ import {
 export default [
   layout("routes/monuhome.tsx", [
     index("routes/dashboardRoute.tsx"), // Default home route (SSR)
-    route("company", "routes/companyRoute.tsx"), // Company route
+    route("company", "features/company/pages/companyPage.tsx"),
     route("test", "routes/test.tsx"), // Test route
     route("form", "routes/Form.tsx"), // Form route
 
