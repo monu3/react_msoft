bf306da904623820315c1ad2b60080f771e9a54d
SUMIT-STHA195 <sumitshrestha2313@gmail.com>
2025-01-08 14:57:54 +0545

    used common layouts

--- app/common/utils/routes/breadcrumbs.ts
+++ app/common/utils/routes/breadcrumbs.ts
@@ -1,7 +1,15 @@
+import { Label } from "flowbite-react";
 import type Dashboard from "~/features/dashboard/components/Dashboard";
 
 export const breadcrumbs = {
   Dashboard: [{ label: "Dashboard", href: "/" }],
   landingPage: [{ label: "Dashboard", href: "/" }, { label: "Landing Page" }],
-  company: [{ label: "Dashboard", href: "/" }, { label: "Company" }],
+  settings: [
+    { label: "Dashboard", href: "/" },
+     { label: "Settings" }
+    ],
+    brandsetting:[
+      {label: "Dashboard", href: "/" },
+      { label: "BrandSetting", href: "/" }
+    ]
 };
