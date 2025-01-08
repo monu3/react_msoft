4d0688025fd3008be33b270d9ebafc2c5513ecc5
Diwash <dopkhrel111@gmail.com>
2025-01-08 14:11:04 +0545

    making applicable for breadCrumbs

--- app/common/utils/routes/breadcrumbs.ts
+++ app/common/utils/routes/breadcrumbs.ts
@@ -3,4 +3,5 @@ import type Dashboard from "~/features/dashboard/components/Dashboard";
 export const breadcrumbs = {
   Dashboard: [{ label: "Dashboard", href: "/" }],
   landingPage: [{ label: "Dashboard", href: "/" }, { label: "Landing Page" }],
+  company: [{ label: "Dashboard", href: "/" }, { label: "Company" }],
 };
--- app/features/company/pages/companyPage.tsx
+++ app/features/company/pages/companyPage.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import Company from "../components/CompanyList";
+import { Breadcrumb } from "flowbite-react";
+import { breadcrumbs } from "~/common/utils/routes/breadcrumbs";
+import BreadcrumbLayouts from "~/common/components/BreadcrumbLayouts";
 
 const companyPage = () => {
-  return <Company />;
+  return (
+    <BreadcrumbLayouts breadcrumbItems={breadcrumbs.company}>
+      <Company />
+    </BreadcrumbLayouts>
+  );
 };
 
 export default companyPage;
