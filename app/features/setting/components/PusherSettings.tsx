580ffffec735daf25dbceac1176b691457f0c374
SUMIT-STHA195 <sumitshrestha2313@gmail.com>
2025-01-08 13:28:07 +0545

    changes added in setting

---
+++ app/features/setting/components/PusherSettings.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PusherSettings = () => {
+  return (
+    <div>This is pusher Settings.</div>
+  )
+}
+
+export default PusherSettings
--- app/features/setting/pages/SettingPage.tsx
+++ app/features/setting/pages/SettingPage.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import SettingBar from '../components/SettingBar'
+import { Outlet } from 'react-router'
 const SettingPage = () => {
   return (
+    <>
+    <div className='flex'>
     <SettingBar/>
+    <div className="flex-grow p-6 overflow-y-auto bg-gray-100">
+            <Outlet />
+          </div>
+    </div>
+      
+    </>
   )
 }
 
