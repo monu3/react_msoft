dc4b9941bc78164477c1a02afe07a22c84087911
SUMIT-STHA195 <sumitshrestha2313@gmail.com>
2025-01-08 15:07:41 +0545

    solved conflict

--- app/common/utils/routes/breadcrumbs.ts
+++ app/common/utils/routes/breadcrumbs.ts
@@ -4,6 +4,7 @@ import type Dashboard from "~/features/dashboard/components/Dashboard";
 export const breadcrumbs = {
   Dashboard: [{ label: "Dashboard", href: "/" }],
   landingPage: [{ label: "Dashboard", href: "/" }, { label: "Landing Page" }],
+  company: [{ label: "Dashboard", href: "/" }, { label: "Company" }],
   settings: [
     { label: "Dashboard", href: "/" },
      { label: "Settings" }
