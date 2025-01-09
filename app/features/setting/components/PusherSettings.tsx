a6f89c0581b9e845d8202e171fecbbbd9a7e0489
SUMIT-STHA195 <sumitshrestha2313@gmail.com>
2025-01-09 15:28:57 +0545

    modified page layout and added email settings, PusherSettings

--- app/features/setting/components/PusherSettings.tsx
+++ app/features/setting/components/PusherSettings.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { Button } from "flowbite-react";
+import React from "react";
+import {
+  FormContainer,
+  FormGrid,
+  FormField,
+} from "~/common/components/FormLayout";
 
 const PusherSettings = () => {
   return (
-    <div>This is pusher Settings.</div>
-  )
-}
+    <FormContainer title="Pusher Settings" description="Edit Pusher Settings">
+      <form className="space-y-6">
+        <FormGrid>
+          <div className="space-y-6">
+            <FormField label="Pusher App Id" id="pusher-app" defaultValue="" />
+            <FormField
+              label="Pusher App Secret"
+              id="pusher-app-secret"
+              defaultValue=""
+            />
+          </div>
+          <div className="space-y-6">
+            <FormField
+              label="Pusher App Key"
+              id="pusher-app-key"
+              defaultValue=""
+            />
+            <FormField
+              label="Pusher App Cluster"
+              id="pusher-app-cluster"
+              defaultValue=""
+            />
+          </div>
+        </FormGrid>
 
-export default PusherSettings
+        <div className="flex justify-end">
+          <Button color="warning">Save Changes</Button>
+        </div>
+      </form>
+    </FormContainer>
+  );
+};
+
+export default PusherSettings;
