5ad2f7fa716376a92a442fbb082b47bb05799f17
SUMIT-STHA195 <sumitshrestha2313@gmail.com>
2025-01-10 17:01:22 +0545

    payment setting added

--- app/features/setting/components/PaymentSetting.tsx
+++ app/features/setting/components/PaymentSetting.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { Button, ToggleSwitch } from "flowbite-react";
+import React, { useState } from "react";
+import {
+  FormContainer,
+  FormField,
+  FormGrid,
+} from "~/common/components/FormLayout";
 
 const PaymentSetting = () => {
+  const [switch1, setSwitch1] = useState(false);
   return (
-    <div>This is PaymentSetting. </div>
+    <FormContainer
+      title="Payment Setting"
+      description="Edit your payment setting"
+    >
+      <form action="">
+        <FormGrid>
+          <div className="space-y-6">
+            <FormField label="Currency" id="currency" defaultValue="NPR" />
+          </div>
+          <div className="space-y-6">
+            <FormField
+              label="Currency Symbol"
+              id="currency-symbol"
+              defaultValue="Rs."
+            />
+          </div>
+        </FormGrid>
+        <div className="flex justify-between mt-4 rounded-md p-3 bg-brand-light">
+          <p>ESEWA</p>
+          <ToggleSwitch
+            checked={switch1}
+            onChange={setSwitch1} 
+          />
+        </div>
+        <div className="flex flex-wrap items-center justify-end gap-4 my-4">
+          <Button color="warning">Save Changes</Button>
+        </div>
+      </form>
+    </FormContainer>
   )
 }
 
-export default PaymentSetting
+export default PaymentSetting;
